685b584f777d7303dc3a24847f2e9ea8623d8c77
Yang Fan <fan.yang@rea-group.com>
2017-03-09 17:31:57 +0800

    add api design doc

--- src/main/java/hello/Greeting.java
+++ src/main/java/hello/Greeting.java
@@ -1,17 +1,16 @@
 package hello;
 
-public class Greeting {
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import org.springframework.hateoas.ResourceSupport;
 
-    private final long id;
+public class Greeting extends ResourceSupport {
+
     private final String content;
 
-    public Greeting(long id, String content) {
-        this.id = id;
+    @JsonCreator
+    public Greeting(@JsonProperty("content") String content) {
         this.content = content;
-    }
-
-    public long getId() {
-        return id;
     }
 
     public String getContent() {
